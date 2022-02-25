7d6339e9c9bd79316f3ca38562575b57f87fc48e
alissatroiano <alissatroianonyc@gmail.com>
2022-02-24 21:54:36 -0500

    Add JavaScriptt function to create tile divs and use flexbox to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,10 @@ const keyboard = document.querySelector('.key-container')
 const keys = [
 	'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
 	'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
-	'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+	'Z', '<<', 'X', 'C', 'V', 'B', 'N', 'M', 'Enter'
 ]
+
+// console.log(keys[27])
 
 const guessRows = [
 	['', '', '', '', ''],
@@ -17,6 +19,19 @@ const guessRows = [
 	['', '', '', '', ''],
 	['', '', '', '', '']
 ]
+
+
+// A function to create a div for each guessRow in the guessRows array
+guessRows.forEach(row => {
+	const rowDiv = document.createElement('div')
+	rowDiv.classList.add('guess-row')
+	tileDisplay.appendChild(rowDiv)
+	row.forEach(tile => {
+		const tileDiv = document.createElement('div')
+		tileDiv.classList.add('guess-tile')
+		rowDiv.appendChild(tileDiv)
+	})
+})
 
 const handleClick = () => {
 	console.log('clicked')
